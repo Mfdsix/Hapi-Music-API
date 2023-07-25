cb10af21e6cb2e85a6daab9bc29cde94dc99b925
Mahfudz Ainur Rif'an <mfdsix.1nd0@gmail.com>
2023-07-26 00:37:51 +0700

    fix migraiton

--- migrations/1690304489249_add-album-likes-fk-constraint.js
+++ migrations/1690304489249_add-album-likes-fk-constraint.js
@@ -1,13 +1,19 @@
 const tableName = 'album_likes'
 const fkUserName = 'fk_album_likes.user'
 const fkAlbumName = 'fk_album_likes.album'
+const uniqueUserName = 'unique_album_likes.user'
+const uniqueAlbumName = 'unique_album_likes.album'
 
 exports.up = pgm => {
+  pgm.addConstraint(tableName, uniqueUserName, 'UNIQUE(user_id)')
   pgm.addConstraint(tableName, fkUserName, 'FOREIGN KEY(user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE')
+  pgm.addConstraint(tableName, uniqueAlbumName, 'UNIQUE(album_id)')
   pgm.addConstraint(tableName, fkAlbumName, 'FOREIGN KEY(album_id) REFERENCES albums(id) ON UPDATE CASCADE ON DELETE CASCADE')
 }
 
 exports.down = pgm => {
   pgm.dropConstraint(tableName, fkUserName)
+  pgm.dropConstraint(tableName, uniqueUserName)
   pgm.dropConstraint(tableName, fkAlbumName)
+  pgm.dropConstraint(tableName, uniqueAlbumName)
 }
